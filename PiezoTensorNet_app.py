68fb93fda8b98cad7eb22555d8737b3a8037b8da
Sachin Poudel <98536450+Sachinscnpdl@users.noreply.github.com>
2023-06-15 21:23:13 +0200

    Update PiezoTensorNet_app.py

--- PiezoTensorNet_app.py
+++ PiezoTensorNet_app.py
@@ -228,3 +228,6 @@ if crystal_rotations:
     
     crystal_plot, max_e, max_theta, max_psi, phi_vals = tensor_rotation_plot(my_tensor, phi = phi, order=[i-1,j-1])
     st.plotly_chart(crystal_plot)
+    
+    rot_optimization = tensor_rotation_optimization(my_tensor, phi = phi, order=[i-1,j-1])
+    st.plotly_chart(rot_optimization)
